7e85cfe1ff823eaa543e14269bdcad01250d042d
ryley/home <33923649+ryle-e@users.noreply.github.com>
2025-11-19 01:06:08 +1100

    added tuning fork and stations room

---
+++ radio game/Assets/Scripts/StationsRoomManager.cs
@@ -0,0 +1,22 @@
+using NUnit.Framework;
+using RyleRadio.Components;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StationsRoomManager : MonoBehaviour
+{
+    [SerializeField] private List<RadioBroadcaster> broadcasters;
+
+    [SerializeField] private Transform forkTransform;
+
+    public void UpdateForkPowers()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
